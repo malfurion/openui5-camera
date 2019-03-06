133601836eda29e541d0462d949e30f76209c177
Mathieu Roll <mathieu.roll@itizzimo.com>
2019-03-06 17:36:26 +0100

    Changed Namespace and removed Depenceny to sap.m

--- src/openui5/camera/CameraRenderer.js
+++ src/openui5/camera/CameraRenderer.js
@@ -1,42 +1,32 @@
 sap.ui.define([],
-	          function() {
-	              "use strict";
+	function() {
+		"use strict";
+		var CameraRenderer = {};
 
-	              /**
-	               * @namespace openui5.camera
-	               */
-	              var CameraRenderer = {};
+		CameraRenderer.render = function(oRm, oControl) {
+			oRm.write("<div");
+			oRm.writeControlData(oControl);
+			oRm.writeClasses();
+			oRm.writeStyles();
+			oRm.write(">");
+			oRm.write("<div style='display: flex; flex-direction: row; align-items: center; justify-content: space-around;'>");
+			oRm.write(
+				"<video playsInline width='%w' height='%h' style='width: %pwpx; height: %phpx;'></video>"
+				.replace("%w", oControl.getVideoWidth())
+				.replace("%h", oControl.getVideoHeight())
+				.replace("%pw", oControl.getWidth())
+				.replace("%ph", oControl.getHeight())
+			);
+			oRm.write(
+				"<canvas width='%w' height='%h' style='display: none; width: %pwpx; height: %phpx;'></canvas>"
+				.replace("%w", oControl.getVideoWidth())
+				.replace("%h", oControl.getVideoHeight())
+				.replace("%pw", oControl.getWidth())
+				.replace("%ph", oControl.getHeight()));
+				oRm.write("</div>");
+				oRm.write("</div>");
+			};
 
-	              /**
-	               * Renders the HTML for the control, using the provided {@link sap.ui.core.RenderManager}.
-	               *
-	               * @param {sap.ui.core.RenderManager} oRm RenderManager object
-	               * @param {sap.ui.core.Control} oControl An object representation of the control that will be rendered
-	               */
-	              CameraRenderer.render = function(oRm, oControl) {
-		              oRm.write("<div");
-		              oRm.writeControlData(oControl);
-		              oRm.writeClasses();
-		              oRm.writeStyles();
-		              oRm.write(">");
-                      oRm.write("<div style='display: flex; flex-direction: row; align-items: center; justify-content: space-around;'>");
-                      oRm.write(
-                          "<video playsInline width='%w' height='%h' style='width: %pwpx; height: %phpx;'></video>"
-                              .replace("%w", oControl.getVideoWidth())
-                              .replace("%h", oControl.getVideoHeight())
-                              .replace("%pw", oControl.getWidth())
-                              .replace("%ph", oControl.getHeight())
-                      );
-                      oRm.write(
-                          "<canvas width='%w' height='%h' style='display: none; width: %pwpx; height: %phpx;'></canvas>"
-                              .replace("%w", oControl.getVideoWidth())
-                              .replace("%h", oControl.getVideoHeight())
-                              .replace("%pw", oControl.getWidth())
-                              .replace("%ph", oControl.getHeight()));
-		              oRm.write("</div>");
-		              oRm.write("</div>");
-	              };
+			return CameraRenderer;
 
-	              return CameraRenderer;
-
-              }, /* bExport= */ true);
+		}, /* bExport= */ true);
--- src/openui5/camera/library.js
+++ src/openui5/camera/library.js
@@ -1,10 +1,3 @@
-/*!
- * ${copyright}
- */
-
-/**
- * Initialization Code and shared classes of library it.designfuture.qrcode.
- */
 sap.ui.define([
 	'jquery.sap.global',
 	'sap/ui/core/library' // library dependency
@@ -12,26 +5,16 @@ sap.ui.define([
 
 	"use strict";
 
-	/**
-	 * Suite controls library.
-	 *
-	 * @namespace
-	 * @name openui5.camera
-	 * @author Tiago Almeida <jumpifzero@gmail.com>
-	 * @version ${version}
-	 * @public
-	 */
-
 	// delegate further initialization of this library to the Core
 	sap.ui.getCore().initLibrary({
-		name : "openui5.camera",
+		name : "me.malfurion.camera",
 		noLibraryCSS: true,
 		version: "${version}",
-		dependencies : ["sap.ui.core", "sap.m"],
+		dependencies : ["sap.ui.core"],
 		types: [],
 		interfaces: [],
 		controls: [
-			"openui5.camera.Camera"
+			"me.malfurion.camera.Camera"
 		],
 		elements: []
 	});
